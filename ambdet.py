1a7a1292c6b04fb05fdce25f54a4902a3ab79a5f
NOTE46 <sahilpaul1177@gmail.com>
2026-04-08 20:00:54 +0530

    added ambulance detection file

---
+++ ambdet.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+model_v=r'Model\best.pt'
+model=YOLO(model_v)
+def ambulance_detect(frame):
+
+    results=model(frame)
+
+    for box in results[0].boxes:
+        label=results[0].names[int(box.cls)]
+
+        if label == 'ambulace':
+            return True
+        else:
+            return False
+        
+    
