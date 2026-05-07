6abe25c900745216a042e7bf43010a429c69121c
NOTE46 <sahilpaul1177@gmail.com>
2026-05-07 08:58:19 +0530

    refactored and optimized the resource c of camera

--- cvfile.py
+++ cvfile.py
@@ -1,30 +1,34 @@
 import cv2 as cv
 from uafile import uamodel
-from control import controlfun
 
-def cvfun(lane_no):
+FRAME_S=5
 
-    vid=cv.VideoCapture(lane_no)
+class Cameraf:
+    def __init__(self,lane_no,source):
+        self.lane_no=lane_no
+        self.vid=cv.VideoCapture(source)
 
-    while True:
+        if not self.vid.isOpened():
+            raise RuntimeError(f"Camera failed on {lane_no}")
+        
+    def cvfun(self):
 
-        for i in range(5):
-            vid.grab()
+        for i in range(FRAME_S  ):
+            self.vid.grab()
 
-        rev,frame=vid.retrieve()
+        rev,frame=self.vid.retrieve()
         
         if rev is False:
-            print(f"Video capture failed on lane {lane_no}")
-            vid.release()
+            print(f"Video capture failed on lane {self.lane_no}")
             return {"density": 0, "ambulance": False}
 
         data=uamodel(frame) 
-        
-        vid.release()
-        cv.destroyAllWindows()
-    
         return data
-        
+    
+    def releasevid(self):
+        self.vid.release()
+
+
         
     
 
