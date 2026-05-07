79daaef8bba6da33a735f0c006c66fbbe157d07a
NOTE46 <sahilpaul1177@gmail.com>
2026-05-07 08:56:01 +0530

    bugfix where path of the model is missing

--- uafile.py
+++ uafile.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 from collections import defaultdict
-import cv2 as cv
 from ambdet import ambulance_detect
+from pathlib import Path
 #vehiclecount to count number of indivitual vehicles and density for how many vehicles
 
-model_path=r'Model\yolov8n.pt'
+model_path=Path("Model")/"yolov8n.pt"
 model = YOLO(model_path)
 
 #read the video from camera and give feedback of what types of object present
@@ -12,7 +12,6 @@ def uamodel(frame):
         
         truckd=False
         vehiclecount=defaultdict(int)
-        global model
 
         #process per frame and init density to count number of objects
         results=model(frame)
